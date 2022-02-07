204ecdeb5ec82561e9de98ed146a78c81b6ed4ea
aginamena5 <aginamena5@gmail.com>
2022-02-07 06:26:27 -0500

    heroku server

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -33,8 +33,9 @@ userRoute.post("/login", async (req, res) => {
 
 //get all users of the site
 userRoute.get("/", async (req, res) => {
-    const allUsers = await User.find();
-    res.send(allUsers);
+    // const allUsers = await User.find();
+    // res.send(allUsers);
+    res.json("reached here")
 })
 
 //get user by id
